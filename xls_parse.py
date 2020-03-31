1cd1fbf9d15ccc62267681af44364671bf12cf1e
Sunil Muthuswamy <sunil_karthik@yahoo.com>
2020-03-30 19:36:00 -0700

    now the program can break on whitespace as well
    Signed-off-by: Sunil Muthuswamy <sunil_karthik@yahoo.com>

--- xls_parse.py
+++ xls_parse.py
@@ -22,7 +22,7 @@ input_dic_parsed = nested_dict(3, list)
 # A map that track the output sheet number for a given color
 color_to_sheet_num_map = {}
 
-# A record of all the colors seen to track the column index
+# A record of all the colors seen, to track the column index
 # for any given color, for the output.
 # Requirement: Each unique color in an input column should go
 # to the same column in the output.
@@ -30,41 +30,52 @@ cur_column_per_color = {}
 
 # Parse the given sheet from the input workbook and store
 # the data in the parsed dictionary.
-def parse_input_workbook(in_wb, sheet, sheet_num):
+# break_on_white - break the color into a new column when seeing
+#  a white space.
+def parse_input_workbook(in_wb, sheet, sheet_num, break_on_white):
    from xlrd.sheet import ctype_text
    # Get the total number of rows and columns in this sheet
    rows, cols = sheet.nrows, sheet.ncols
    # print "Number of rows: %s   number of cols: %s" % (rows, cols)
    for col in range(0, cols):  # Iterate through columns in this sheet
-      color_seen_in_this_row = {}
-      for row in range(0, rows):  # Iterate through the rows in this column
+      color_seen_in_this_col = {}
+      # For each column, iterate through the rows in that column
+      last_non_white_bgx = -1
+      white_space_encountered = False
+      for row in range(0, rows):
          col_type = sheet.cell_type(row, col)
-         if col_type is xlrd.XL_CELL_EMPTY:
-            continue # skip empty cells
          # Get cell details of [row, col]
          cell_obj = sheet.cell(row, col)
          xfx = sheet.cell_xf_index(row, col)
          xf = in_wb.xf_list[xfx]
          bgx = xf.background.pattern_colour_index
+         if col_type is xlrd.XL_CELL_EMPTY or cell_obj == '' or bgx == 64:
+            logging.debug('[%d,%d] empty cell' % (row + 1, col + 1))
+            white_space_encountered = True
+            continue # skip empty cells
+         #if bgx == 64:
+         #   logging.debug('[%d,%d] cell_obj: [%s] white cell, skipping...' % (row + 1, col + 1, cell_obj))
+         #   continue # skip white
+         if break_on_white and white_space_encountered and last_non_white_bgx != -1:
+               logging.debug('breaking on white for color: %d' % (last_non_white_bgx))      
+               cur_column_per_color[last_non_white_bgx] += 1
+         last_non_white_bgx =  bgx
+         white_space_encountered = False
          # record all the colors that are seen in this column
-         if bgx not in color_seen_in_this_row.keys():
-            color_seen_in_this_row[bgx] = 1
+         if bgx not in color_seen_in_this_col.keys():
+            color_seen_in_this_col[bgx] = 1
          if bgx not in cur_column_per_color.keys():
             cur_column_per_color[bgx] = -1
          column = cur_column_per_color[bgx]
          column += 1
          color_map = in_wb.colour_map[bgx]
-         if bgx == 64:
-            continue # skip white
-         if bgx not in color_seen_in_this_row.keys():
-            color_seen_in_this_row[bgx]
          logging.debug('[%s,%s] cell_obj: [%s] [%s], column: %d' % (row + 1, col + 1, cell_obj, bgx, column))
          input_dic_parsed[sheet_num][bgx][column].append(cell_obj.value)
-      logging.debug('colors in this col %s', color_seen_in_this_row)
+      logging.debug('colors in this col %s', color_seen_in_this_col)
       # for all the colors that were there in this column, increment
       # the column count, so that the next hit of this color can
       # go to the next output column.
-      for colors in color_seen_in_this_row:
+      for colors in color_seen_in_this_col:
          cur_column_per_color[colors] += 1
       logging.debug("current column per color %s" % (cur_column_per_color))
    logging.debug('parsed dict %s' % (input_dic_parsed))
@@ -103,36 +114,47 @@ def print_help():
 def main(argv):
    inputfile = ''
    outputfile = ''
+   should_break_on_white = 'n'
+   break_on_white = False
    try:
       opts, args = getopt.getopt(argv,"vhi:o:",["ifile=","ofile="])
    except getopt.GetoptError:
       print_help()
-   if (len(sys.argv) == 1):
-      inputfile = input("Enter the name of the input [.xls] file: ")
-      # strip the quotes at the start and end, else
-      # paths with white spaces won't work.
-      inputfile = inputfile.strip('\"')
-      outputfile = input("Enter the name of the output [.xlsx/.xls] file: ")
-      input("Warning: Program will overwrite the output file if it exists. Hit Enter to continue or Ctrl+C to exit.")
-   else:
-      for opt, arg in opts:
-         if opt == '-h':
-            print_help()
-         elif opt in ("-i", "--ifile"):
-            inputfile = arg
-         elif opt in ("-o", "--ofile"):
-            outputfile = arg
-         elif opt in ("-v"):
-            logging.basicConfig(level=logging.DEBUG)
-         else:
-            print_help()
+   for opt, arg in opts:
+      if opt == '-h':
+         print_help()
+      elif opt in ("-i", "--ifile"):
+         inputfile = arg
+      elif opt in ("-o", "--ofile"):
+         outputfile = arg
+      elif opt in ("-v"):
+         logging.basicConfig(level=logging.DEBUG)
+      else:
+         print_help()
 
+   print("This program will break the input on new column and new sheet per color.")
+   # get the input file if one is not provided.
+   if inputfile == '':
+      inputfile = input("Enter the name of the input [.xls] file: ")
+
+   # strip the quotes at the start and end, else
+   # paths with white spaces won't work.
+   inputfile = inputfile.strip('\"')
+
+   # get the output file if one is not provided.
+   if outputfile == '':
+      outputfile = input("Enter the name of the output [.xlsx] file: ")
+   should_break_on_white = input("Do you want to break on whitespace as well (y/n)? ")
+   if (should_break_on_white == 'y' or should_break_on_white == 'Y'):
+      logging.info('OK, will break the column on whitespace')
+      break_on_white = True
+   input("Warning: Program will overwrite the output file if it exists. Hit Enter to continue or Ctrl+C to exit.")
    in_wb = xlrd.open_workbook(inputfile, formatting_info=True)
    nsheets = in_wb.nsheets
    for sheet_num in range(0, nsheets):
       sheet = in_wb.sheet_by_index(sheet_num)
       logging.debug('Sheet name: %s' % sheet.name)
-      parse_input_workbook(in_wb, sheet, sheet_num)
+      parse_input_workbook(in_wb, sheet, sheet_num, break_on_white)
 
    out_wb = xlsxwriter.Workbook(outputfile)
    logging.debug('total distinct sheets = %s', len(input_dic_parsed))
